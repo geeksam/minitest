d470b72808e2654befb4a5582c0b1bb2eceaf01a
Sam Livingston-Gray <geeksam@gmail.com>
2012-01-06 21:48:20 -1000

    delegate and inline

--- lib/minitest/describe_it.rb
+++ lib/minitest/describe_it.rb
@@ -4,33 +4,33 @@ require 'minitest/spec'
 
 module MiniTest
   def self.obfuscate_expectation_methods
+    MiniTest::Expectations.obfuscate_expectation_methods
+  end
+
+  def self.elucidate_expectation_methods
+    MiniTest::Expectations.elucidate_expectation_methods
+  end
+end
+
+module MiniTest::Expectations
+  def self.obfuscate_expectation_methods
     MiniTest::Expectations.instance_methods.each do |method|
-      next if method =~ /^_(.*)/
-      MiniTest::Expectations.obfuscate method
+      next if method =~ /^_(must|wont)_/
+      original   = method.to_sym
+      obfuscated = ('_%s' % method.to_s).to_sym
+      alias :"#{obfuscated}" :"#{original}"
+      remove_method original
     end
   end
 
   def self.elucidate_expectation_methods
     MiniTest::Expectations.instance_methods.each do |method|
-      next unless method =~ /^_(.*)/
-      MiniTest::Expectations.elucidate(method)
+      next unless method =~ /^_(must|wont)_/
+      original   = method.to_s.gsub(/^_/, '').to_sym
+      obfuscated = method.to_sym
+      alias :"#{original}" :"#{obfuscated}"
+      remove_method obfuscated
     end
-  end
-end
-
-module MiniTest::Expectations
-  def self.obfuscate(method)
-    original   = method.to_sym
-    obfuscated = ('_' + method.to_s).to_sym
-    alias :"#{obfuscated}" :"#{original}"
-    remove_method original
-  end
-
-  def self.elucidate(method)
-    original   = method.to_s.gsub(/^_/, '').to_sym
-    obfuscated = method.to_sym
-    alias :"#{original}" :"#{obfuscated}"
-    remove_method obfuscated
   end
 end
 
